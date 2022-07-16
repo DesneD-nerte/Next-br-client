83a6d265f6569fc4d0bb862ce786fd05ef91e44f
Alexandr Ershov <ershov.avdd@gmail.com>
2022-07-17 00:34:13 +0700

    Added Arrow Buttons

--- components/MainGallery/MainGallery.tsx
+++ components/MainGallery/MainGallery.tsx
@@ -2,22 +2,45 @@ import Image from "next/image";
 import { relative } from "path";
 import React from "react";
 import ImageGallery from "./components/ImageGallery";
+import styles from "./MainGallery.module.css";
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
+import { IconButton } from "@mui/material";
 
 function MainGallery() {
     return (
-        <div style={{ position: "relative" }}>
+        <div className={styles.mainComponent}>
             <button
-                style={{ position: "absolute", top: "50%" }}
+                className={`${styles.button} ${styles.buttonLeft}`}
                 onClick={(e) => console.log(123)}
             >
-                Left
+                <svg
+                    stroke="white"
+                    width="50px"
+                    height="75px"
+                    strokeWidth="1.5"
+                    fill="none"
+                >
+                    <path d="M5 40 L30 10" />
+                    <path d="M5 40 L30 70" />
+                </svg>
             </button>
             <ImageGallery></ImageGallery>
             <button
-                style={{ position: "absolute", top: "50%", right: 0 }}
+                className={`${styles.button} ${styles.buttonRight}`}
                 onClick={(e) => console.log(123)}
             >
-                Right
+                <div>
+                    <svg
+                        stroke="white"
+                        width="50px"
+                        height="75px"
+                        strokeWidth="1.5"
+                        fill="none"
+                    >
+                        <path d="M45 40 L20 10" />
+                        <path d="M45 40 L20 70" />
+                    </svg>
+                </div>
             </button>
         </div>
     );
