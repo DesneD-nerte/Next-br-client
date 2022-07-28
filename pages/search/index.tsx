bd9dacac623f48c8033e4b521b2b6b21655dcfbc
Alexandr Ershov <ershov.avdd@gmail.com>
2022-07-28 23:53:34 +0700

    start working with search grid

--- pages/search/index.tsx
+++ pages/search/index.tsx
@@ -1,10 +1,66 @@
 import type { NextPage } from "next";
+import Link from "next/link";
 import WithoutGallery from "../../components/layout/WithoutGallery";
+import styles from "./search.module.css";
+import Image from "next/image";
 
 const Search: NextPage = () => {
     return (
         <WithoutGallery isMargin={false}>
-            <div></div>
+            <>
+                <div className={styles.controlContainer}>Control panel</div>
+                <div className={styles.searchContainer}>Search</div>
+                <div className={styles.itemsContainer}>
+                    <Link href="/">
+                        <a>
+                            <Image
+                                src="/landscape/1.jpg"
+                                width={60}
+                                height={60}
+                                alt="image"
+                                layout="responsive"
+                                // objectFit="contain"
+                            ></Image>
+                        </a>
+                    </Link>
+                    <Link href="/">
+                        <a>
+                            <Image
+                                src="/landscape/2.jpg"
+                                width={60}
+                                height={60}
+                                alt="image"
+                                layout="responsive"
+                                // objectFit="contain"
+                            ></Image>
+                        </a>
+                    </Link>
+                    <Link href="/">
+                        <a>
+                            <Image
+                                src="/landscape/3.jpg"
+                                width={60}
+                                height={60}
+                                alt="image"
+                                layout="responsive"
+                                // objectFit="contain"
+                            ></Image>
+                        </a>
+                    </Link>
+                    <Link href="/">
+                        <a>
+                            <Image
+                                src="/landscape/4.jpg"
+                                width={60}
+                                height={60}
+                                alt="image"
+                                layout="responsive"
+                                // objectFit="contain"
+                            ></Image>
+                        </a>
+                    </Link>
+                </div>
+            </>
         </WithoutGallery>
     );
 };
