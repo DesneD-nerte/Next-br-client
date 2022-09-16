968b9b3e71ae52edd9054a16034622835b053a26
Alexandr Ershov <ershov.avdd@gmail.com>
2022-09-16 19:09:18 +0700

    Added Inputs and login form

--- pages/signIn/index.tsx
+++ pages/signIn/index.tsx
@@ -1,12 +1,21 @@
-import React from "react";
-import MainLayout from "../../components/layout/MainLayout";
+import React, { useEffect } from "react";
+import LoginForm from "../../components/forms/auth/login/LoginForm";
+import MainContent from "../../components/layout/MainContent";
+import { useAppDispatch } from "../../store/hooks";
+import { setExpandMenu } from "../../store/reducers/MenuSlice";
 
-function index() {
+function SignIn() {
+    const dispatch = useAppDispatch();
+
+    useEffect(() => {
+        dispatch(setExpandMenu(false));
+    }, []);
+
     return (
-        <MainLayout>
-            <div>123</div>
-        </MainLayout>
+        <MainContent isMargin={false}>
+            <LoginForm></LoginForm>
+        </MainContent>
     );
 }
 
-export default index;
+export default SignIn;
