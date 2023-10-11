2aab89e11d58eb61eeb7b2efc850d215e684f915
Aleksandr Ershov <ershov_a@joy.money>
2023-10-12 01:47:42 +0700

    cleaning

--- app/[lng]/layout.tsx
+++ app/[lng]/layout.tsx
@@ -16,12 +16,12 @@ interface Props {
 export default function LanguageLayout({ children, params: { lng } }: Props) {
     return (
         <html lang={lng} dir={dir(lng)}>
-            {/* <MainLayout> */}
-            <>
-                <head />
-                <body>{children}</body>
-            </>
-            {/* </MainLayout> */}
+            <MainLayout>
+                <>
+                    <head />
+                    <body>{children}</body>
+                </>
+            </MainLayout>
         </html>
     );
 }
